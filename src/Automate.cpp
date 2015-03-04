b230965132f5c9a2cc99ecff882b3910af37405e
jeje42 <jerome.guidon@insa-lyon.fr>
2015-03-04 22:36:02 +0100

    nouveau fichier : src/Automate.cpp 	nouveau fichier : src/Automate.h

---
+++ src/Automate.cpp
@@ -0,0 +1,14 @@
+#include "Automate.h"
+
+#include <iostream>
+
+Automate::Automate()
+{
+    //ctor
+    std::cout << "Construction de l'automate" << std::endl;
+}
+
+Automate::~Automate()
+{
+    //dtor
+}
---
+++ src/Automate.h
@@ -0,0 +1,14 @@
+#ifndef AUTOMATE_H
+#define AUTOMATE_H
+
+
+class Automate
+{
+    public:
+        Automate();
+        virtual ~Automate();
+    protected:
+    private:
+};
+
+#endif // AUTOMATE_H
