11cb9694002789d9c4e5824daf075fc9fe595462
jeje42 <jerome.guidon@insa-lyon.fr>
2015-03-04 22:35:17 +0100

    modifié :         CMakeLists.txt 	modifié :         src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
+#include "Automate.h"
+
 using namespace std;
 
 int main(int argc, char** argv)
 {
 	cout << "Hello world" << endl;
+
+	Automate *automate = new Automate();
 
 	return 0;
 }
