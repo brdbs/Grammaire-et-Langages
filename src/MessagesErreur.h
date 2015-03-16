4c099947a67fd5798f5019421f2885b7a38174da
jeje42 <jerome.guidon@insa-lyon.fr>
2015-03-16 09:39:41 +0100

    modifié :         src/MessagesErreur.h

--- src/MessagesErreur.h
+++ src/MessagesErreur.h
@@ -1,3 +1,13 @@
 /*
  * Destiné à contenir l'ensemble des messages d'erreurs que peut renvoyer le programme
  */
+
+#include <string>
+
+const string NoArgument = "Erreur, veuillez specifier des arguments\n
+  Utilisation :\n
+    ../lut [-p] [-a] [-e] [-o] source.lt\n
+      [-p] affiche le code source reconnu\n
+      [-a] analyse le programme de maniere statique\n
+      [-e] execute interactivement le programme\n
+      [-o] optimise les expressions et instructions\n";
