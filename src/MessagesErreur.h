624612a663f69a1fc992e4c5dd75655923df889a
jeje42 <jerome.guidon@insa-lyon.fr>
2015-03-16 09:48:26 +0100

    modifié :         src/MessagesErreur.h

--- src/MessagesErreur.h
+++ src/MessagesErreur.h
@@ -5,9 +5,11 @@
 #include <string>
 
 const string NoArgument = "Erreur, veuillez specifier des arguments\n
-  Utilisation :\n
-    ../lut [-p] [-a] [-e] [-o] source.lt\n
-      [-p] affiche le code source reconnu\n
-      [-a] analyse le programme de maniere statique\n
-      [-e] execute interactivement le programme\n
-      [-o] optimise les expressions et instructions\n";
+  \tUtilisation :\n
+    \t\t../lut [-p] [-a] [-e] [-o] source.lt\n
+      \t\t\t[-p] affiche le code source reconnu\n
+      \t\t\t[-a] analyse le programme de maniere statique\n
+      \t\t\t[-e] execute interactivement le programme\n
+      \t\t\t[-o] optimise les expressions et instructions\n";
+
+const string fichierInexistant = "";
