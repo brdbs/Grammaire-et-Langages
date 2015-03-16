21dfd5faed234a3956aff7ebd67cdd295e99a1bf
Faugoin Marion <mfaugoin@if501-219-10.insa-lyon.fr>
2015-03-16 09:06:49 +0100

    première implementation du lexer, à tester, non compilée

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -1,5 +1,7 @@
 #include "Lexer.h"
 #include "symbole/Symbole.h"
+#include <boost/regex.hpp>
+
 
 Lexer::Lexer()
 {
@@ -13,4 +15,37 @@ Lexer::~Lexer()
 
 int Lexer::getNext(){
 	return 0;
+	
+	/* boucler sur la lecture de caractere
+		*
+		* rechercher les expreg correspondant totalement ou partiellement aux caractères lus jusqu'a present
+		* 
+		* on lit les caractères jusqu'a ce que plus aucune expreg ne corresponde.
+		* A ce moment la, si l'on avait une expreg correspondait (totalement), on enregistre le symbole associé.
+		* Sinon il y a une erreur.
+		* 
+		* http://www.boost.org/doc/libs/1_31_0/libs/regex/doc/partial_matches.html
+		* 
+	*/
+		
+	carLus = "";
+	char carLu = '';
+	 boost::match_results<std::string::const_iterator> what;
+	while(iss.get(carLu))
+	{
+		if(0 == boost::regex_match(carLus+carLu, what, motCle, boost::match_default | boost::match_partial)) //to customizzzzzzze
+		{
+		
+		
+		}
+	}
+	
+	
+	
+	
+	
+	
 }
+
+
+
--- src/Lexer.h
+++ src/Lexer.h
@@ -11,6 +11,12 @@ class Lexer
 		int getNext();
 	protected:
 	private:
+		istringstream iss;
+		string carLus;
+		boost::regex motCle("var|const|lire|ecrire");
+		boost::regex symbole("-|\+|/|\*|,|;|\(|\)|=|:=");
+		boost::regex nb("[0-9]+");
+		boost::regex id("[a-zA-Z][a-zA-Z0-9]+");
 };
 
 #endif // LEXER_H
