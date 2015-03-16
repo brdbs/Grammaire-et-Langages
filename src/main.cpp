43bac68ed7f8c26b90abc8c9909d6846ef75c565
yukai <yukai.wang1991@gmail.com>
2015-03-16 11:10:43 +0100

    interface des options en ligne de commande

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,86 @@
 #include <iostream>
-#include <boost/regex.hpp>
+#include <boost/regex.hpp>\
 
+#include "Lexer.h"
 #include "Automate.h"
 
 
 using namespace std;
 
-int main(int argc, char** argv)
+int main(int argc, const char * argv[])
 {
 	string cheminFichier;
-	if (argc == 2)
-	{
-		cheminFichier = argv[1];
-	}
-	else
-	{
-		cout << "nom du fichier :";
-		cin >> cheminFichier;
-	}
+	Lexer lexer;
+
+	if (argc == 1) {
+		fprintf(stderr, "Erreur, veuillez specifier des arguments\n
+			\tUtilisation :\n
+			\t\t../lut [-p] [-a] [-e] [-o] source.lt\n
+      \t\t\t[-p] affiche le code source reconnu\n
+      \t\t\t[-a] analyse le programme de maniere statique\n
+      \t\t\t[-e] execute interactivement le programme\n
+      \t\t\t[-o] optimise les expressions et instructions");
+
+		return EXIT_FAILURE;
+	} 
+
+	if (argc >= 2) {
+		cheminFichier = argv[argc-1];
+
+		if (lexer.scannerFichier(cheminFichier) == 0) {
+			for (int i = 1; i < argc-1; i++) {
+				string option = argv[i];
+				if (option.length() == 2 && option[0] == '-') {
+					switch (option[1]) {
+						case 'p':
+							// afficher le code source
+							break;
+
+						case 'a':
+							// analyser le programme de maniere statique
+							break;
+
+						case 'e':
+							// executer le programme
+							break;
+
+						case 'o':
+							//optimiser les expressions et instructions
+							break;
+
+						default:
+							break;
+					}
+				}
+			}
+
+			return EXIT_SUCCESS;
+		} else {
+			// afficher sur sortie d'erruer
+			return EXIT_FAILURE;
+		}
+	} 
+	
 	Automate *automate = new Automate(cheminFichier);
 	string pause;
 	cin >> pause;
 	return 0;
 }
+
+// void analyserLigneCommande(int argc, const char * argv[])
+// {
+// 	string cheminFichier;
+// 	Lexer lexer;
+// 	if (argc == 0) {
+// 		fprintf(stderr, "Erreur, veuillez specifier des arguments\n
+// 			\tUtilisation :\n
+// 			\t\t../lut [-p] [-a] [-e] [-o] source.lt\n
+//       \t\t\t[-p] affiche le code source reconnu\n
+//       \t\t\t[-a] analyse le programme de maniere statique\n
+//       \t\t\t[-e] execute interactivement le programme\n
+//       \t\t\t[-o] optimise les expressions et instructions");
+// 	} else if (argc == 1) {
+// 		cheminFichier = argv[0];
+// 		lexer.scannerFichier(cheminFichier)
+// 	}
+// }
